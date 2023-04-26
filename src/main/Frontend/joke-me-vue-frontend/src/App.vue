327797a7a0e903b88631b025056b66c0de511c08
t-kirschner <tobias.tobi.tt@gmail.com>
2023-04-26 16:26:06 +0200

    We can talk!

---
+++ src/main/Frontend/joke-me-vue-frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import UserInput from './components/UserInput.vue'
+
+export default {
+  components: {
+    UserInput
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>Meine App</h1>
+    <user-input></user-input>
+  </div>
+</template>
---
+++ src/main/Frontend/joke-me-vue-frontend/src/components/UserInput.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      inputText: ''
+    }
+  },
+  methods: {
+    handleClick() {
+      // Hier kannst du die Eingabe aus inputText verwenden
+      axios.post('http://localhost:8080/', {text: this.inputText})
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }
+  }
+}
+</script>
+
+<template>
+    <div>
+      <input type="text" v-model="inputText">
+      <button @click="handleClick">Submit</button>
+    </div>
+  </template>
