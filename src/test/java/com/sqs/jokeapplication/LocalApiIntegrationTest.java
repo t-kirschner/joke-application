f441a44d962be6dda3ed3a03a9bd847cc31ec118
t-kirschner <tobias.tobi.tt@gmail.com>
2023-06-23 16:28:38 +0200

    changed LocalApiIntegrationTest

--- src/test/java/com/sqs/jokeapplication/LocalApiIntegrationTest.java
+++ src/test/java/com/sqs/jokeapplication/LocalApiIntegrationTest.java
@@ -1,14 +1,18 @@
 package com.sqs.jokeapplication;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LocalApiIntegrationTest {
 
     @LocalServerPort
