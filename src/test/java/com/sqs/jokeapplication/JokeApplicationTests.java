3c7020de6388d079c2d210385e4bca9c1c2fea7f
t-kirschner <tobias.tobi.tt@gmail.com>
2023-06-23 12:48:13 +0200

    changed directory of JokeApplicationTests class

--- src/test/java/com/sqs/jokeapplication/JokeApplicationTests.java
+++ src/test/java/com/sqs/jokeapplication/JokeApplicationTests.java
@@ -4,7 +4,7 @@ import com.sqs.jokeapplication.JokeApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = JokeApplication.class)
+@SpringBootTest
 class JokeApplicationTests {
 
 	@Test
