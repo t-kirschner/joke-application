4cf7fd39ac2165b20481b22489c12090a946b4aa
t-kirschner <tobias.tobi.tt@gmail.com>
2023-06-24 13:36:42 +0200

    updated JokeDatabaseIntegrationTest

--- src/test/java/com/sqs/jokeapplication/JokeDatabaseIntegrationTest.java
+++ src/test/java/com/sqs/jokeapplication/JokeDatabaseIntegrationTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
@@ -49,12 +53,33 @@ public class JokeDatabaseIntegrationTest {
 
     @Test
     public void testCountByLanguage() {
+        ArrayList<Joke> jokeList = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Joke jokeGerman = new Joke("Test Witz", Language.GERMAN);
+            Joke jokeEnglish= new Joke("Test Joke", Language.ENGLISH);
+            jokeList.add(jokeGerman);
+            jokeList.add(jokeEnglish);
+        }
+        jokeRepository.saveAll(jokeList);
 
+        assertEquals(jokeRepository.countByLanguage(Language.GERMAN.toString()), 5);
+        assertEquals(jokeRepository.countByLanguage(Language.ENGLISH.toString()), 5);
     }
 
     @Test
     public void testFindFirstByLanguage() {
+        Joke jokeGerman1 = new Joke("Test Witz 1", Language.GERMAN);
+        Joke jokeGerman2 = new Joke("Test Witz 2", Language.GERMAN);
+        Joke jokeEnglish1 = new Joke("Test Joke 1", Language.ENGLISH);
+        Joke jokeEnglish2= new Joke("Test Joke 2", Language.ENGLISH);
 
+        jokeRepository.save(jokeGerman1);
+        jokeRepository.save(jokeGerman2);
+        jokeRepository.save(jokeEnglish1);
+        jokeRepository.save(jokeEnglish2);
+
+        assertEquals(jokeRepository.findFirstByLanguage(Language.GERMAN.toString()), jokeGerman1);
+        assertEquals(jokeRepository.findFirstByLanguage(Language.ENGLISH.toString()), jokeEnglish1);
     }
 }
 
