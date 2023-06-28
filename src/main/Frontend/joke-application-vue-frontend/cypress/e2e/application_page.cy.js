31968509e07f31fed775e4102490b16928cd0865
t-kirschner <tobias.tobi.tt@gmail.com>
2023-06-28 22:59:42 +0200

    updated cypress end to end test

--- src/main/Frontend/joke-application-vue-frontend/cypress/e2e/application_page.cy.js
+++ src/main/Frontend/joke-application-vue-frontend/cypress/e2e/application_page.cy.js
@@ -1,5 +1,32 @@
 describe('The Home Page', () => {
-  it('successfully loads', () => {
+  it('should display a result when submitting valid input', () => {
+    // Besuche die Seite
     cy.visit('http://localhost:5173')
+
+    // Überprüfe die Dropdown-Optionen
+    cy.get('#language-dropdown').select('English')
+    cy.get('#language-dropdown').should('have.value', '1')
+
+    cy.get('.btn').click()
+
+    cy.wait(2000) // Warte 2 Sekunden, um sicherzustellen, dass die API-Antwort erhalten wurde
+    cy.get('#joke-result').should('not.be.empty')
+
+    cy.request('POST', 'http://localhost:8080/', { inputLanguage: '1' }).then(response => {
+      expect(response.status).to.equal(200) // Überprüfe den HTTP-Statuscode
+    })
+
+    // Überprüfe die Dropdown-Optionen
+    cy.get('#language-dropdown').select('German')
+    cy.get('#language-dropdown').should('have.value', '2')
+
+    cy.get('.btn').click()
+
+    cy.wait(2000) // Warte 2 Sekunden, um sicherzustellen, dass die API-Antwort erhalten wurde
+    cy.get('#joke-result').should('not.be.empty')
+
+    cy.request('POST', 'http://localhost:8080/', { inputLanguage: '1' }).then(response => {
+      expect(response.status).to.equal(200) // Überprüfe den HTTP-Statuscode
+    })
   })
 })
